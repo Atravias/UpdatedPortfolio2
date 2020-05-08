16d98255c385c56ed33f8fd70939167c74105d04
Atravias Borley <atravias@gmail.com>
2020-05-08 15:39:01 -0600

    modified js file to save info to localstorage so there was at least SOMETHING the submit button was doing!

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,15 @@
 var submitBox = $('#submit-box');
 var nameBox = $('#name-box');
 var emailBox = $('#email-box');
-var submitBtn = $('#submit')
+var submitBtn = $('#submit');
 
-$(submitBtn).on('click', () => {
-    localStorage.setItem(nameBox.val(), emailBox.val());
-    return;
-
+$(submitBtn).on('click', (event) => {
+    event.preventDefault();
+    localStorage.setItem("Name", nameBox.val().trim());
+    localStorage.setItem("Email", emailBox.val().trim());
+    localStorage.setItem("Comments", submitBox.val());
+    localStorage.setItem("Info", localStorage.getItem("Name") + " " + localStorage.getItem("Email"));
+    var name = localStorage.getItem("Name");
+    var info = localStorage.getItem("Info").valueOf();
+    console.log(info);
 })
